85a41d388f1521753c43bf34c249f30527cbafa8
MilindPSadar <milindsardar007@gmail.com>
2023-07-25 12:25:37 +0530

    [milind]updated listener class --taking screenshot failed TC

--- src/main/java/com/flipkart/qa/listener/Listener.java
+++ src/main/java/com/flipkart/qa/listener/Listener.java
@@ -1,10 +1,15 @@
 package com.flipkart.qa.listener;
 
+import java.io.IOException;
+
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
-public class Listener implements ITestListener {
+import com.flipkart.qa.base.BaseClass;
+
+public class Listener extends BaseClass implements ITestListener {
+	BaseClass b=new BaseClass();
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		ITestListener.super.onTestSuccess(result);
@@ -15,7 +20,13 @@ public class Listener implements ITestListener {
 		// TODO Auto-generated method stub
 		ITestListener.super.onTestFailure(result);
 		Reporter.log("TC "+result.getName()+" is failed please try again", true);
-
+		String fileName=result.getName();
+		try {
+			b.takeScreenshot(fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 @Override
 public void onTestSkipped(ITestResult result) {
